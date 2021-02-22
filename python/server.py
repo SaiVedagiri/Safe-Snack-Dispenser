4c0cea7d2addefccc5f23387484882db423401b0
albertzoom11 <52132982+albertzoom11@users.noreply.github.com>
2021-02-22 18:06:45 -0500

    Update server.py

--- python/server.py
+++ python/server.py
@@ -1,48 +1,20 @@
 import requests
 from flask import Flask, request
-import RPi.GPIO as GPIO
 import time
-from gpiozero import DistanceSensor
+# from gpiozero import DistanceSensor
 
+trigger = False
 app = Flask(__name__)
 
 @app.route('/dispense', methods=['POST'])
 def dispense():
+    global trigger
+    trigger = True
+    return 0
 
-    GPIO.setmode(GPIO.BCM)
-
-    trigger = 18
-    echo = 24
-    
-    #set GPIO direction (IN / OUT)
-    GPIO.setup(trigger, GPIO.OUT)
-    GPIO.setup(echo, GPIO.IN)
-
-    # set Trigger to HIGH
-    GPIO.output(trigger, True)
- 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(trigger, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(echo) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
-    while GPIO.input(echo) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
+while True:
+    if trigger:
+        print('dispense!')
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
