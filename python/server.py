9de2943f79c85d67072fbdf861437adf84f4180f
Albert <albertrzou@gmail.com>
2021-02-22 21:13:54 -0500

    pi code

--- python/server.py
+++ python/server.py
@@ -1,46 +1,16 @@
 import requests
 from flask import Flask, request
-import RPi.GPIO as GPIO
 import time
-from gpiozero import DistanceSensor
+from gpiozero import DistanceSensor, AngularServo
+
+ultrasonic = DistanceSensor(echo=18, trigger=23, max_distance=1, threshold_distance=0.2)
+servo = AngularServo(24)
 
 app = Flask(__name__)
 
 @app.route('/dispense', methods=['POST'])
 def dispense():
-
-    GPIO.setmode(GPIO.BCM)
-
-    trigger = 18
-    echo = 24
     
-    #set GPIO direction (IN / OUT)
-    GPIO.setup(trigger, GPIO.OUT)
-    GPIO.setup(echo, GPIO.IN)
-
-    # set Trigger to HIGH
-    GPIO.output(trigger, True)
- 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(trigger, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(echo) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
-    while GPIO.input(echo) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
  
     return distance
     
